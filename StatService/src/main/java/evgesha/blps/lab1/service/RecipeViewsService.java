26ea8720258359520ee806a4f939f57b002bcec9
3ilib0ba <evonniy@mail.ru>
2023-06-15 17:03:06 +0300

    LAB-3 stat service update transfer from POJO from JMS

--- StatService/src/main/java/evgesha/blps/lab1/service/RecipeViewsService.java
+++ StatService/src/main/java/evgesha/blps/lab1/service/RecipeViewsService.java
@@ -1,14 +1,14 @@
 package evgesha.blps.lab1.service;
 
 import evgesha.blps.lab1.entity.RecipeViews;
-import evgesha.blps.lab1.exception.CantConvertToTextMessageException;
 import evgesha.blps.lab1.repository.RecipeViewsRepository;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
+import org.springframework.util.SerializationUtils;
 
 import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.TextMessage;
+import javax.jms.ObjectMessage;
+import java.util.Objects;
 import java.util.Optional;
 
 
@@ -23,8 +23,11 @@ public class RecipeViewsService {
     }
 
     @JmsListener(destination = "STAT_COUNT_VIEWS")
-    public void receive(final Message message) throws JMSException {
-        Long recipeId = Long.valueOf(convertToTextMessage(message).getText());
+    public void receive(final ObjectMessage message) throws JMSException {
+        Object objectInput = SerializationUtils.deserialize((byte[]) message.getObject());
+        System.out.println(("get new message OBJECT = " + objectInput));
+
+        Long recipeId = Long.valueOf((String) Objects.requireNonNull(objectInput));
 
         Optional<RecipeViews> isRecipeViews = recipeViewsRepository.findById(recipeId);
         RecipeViews recipeViews;
@@ -36,13 +39,6 @@ public class RecipeViewsService {
         recipeViews.setViewsCount(recipeViews.getViewsCount() + 1);
         recipeViewsRepository.save(recipeViews);
 
-    }
-
-    private TextMessage convertToTextMessage(Message message) {
-        if (message instanceof TextMessage textMessage) {
-            return textMessage;
-        }
-        throw new CantConvertToTextMessageException();
     }
 
 }
