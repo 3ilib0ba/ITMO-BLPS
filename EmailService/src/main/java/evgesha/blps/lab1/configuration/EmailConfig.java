0ab7ed3f83c5cb770037e1f7fa6017010ad334fe
Daniil Fridkin <daniilfridkin@gmail.com>
2023-06-15 18:22:07 +0300

    Email sender and little problem

---
+++ EmailService/src/main/java/evgesha/blps/lab1/configuration/EmailConfig.java
@@ -0,0 +1,56 @@
+package evgesha.blps.lab1.configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@Getter
+@Setter
+public class EmailConfig {
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.protocol}")
+    private String protocol;
+
+    @Value("${mail.debug}")
+    private String debug;
+
+    @Bean
+    public JavaMailSender getMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties properties = mailSender.getJavaMailProperties();
+// так было на гите, ниже от AI
+//        properties.setProperty("mail.transport.protocol", protocol);
+//        properties.setProperty("mail.debug", debug);
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
+
--- EmailService/src/main/java/evgesha/blps/lab1/service/EmailService.java
+++ EmailService/src/main/java/evgesha/blps/lab1/service/EmailService.java
@@ -2,8 +2,10 @@ package evgesha.blps.lab1.service;
 
 import evgesha.blps.lab1.dto.EmailWithRecipeDto;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.util.SerializationUtils;
+import org.springframework.mail.SimpleMailMessage;
 
 import javax.jms.JMSException;
 import javax.jms.ObjectMessage;
@@ -11,6 +13,9 @@ import javax.jms.ObjectMessage;
 
 @Service
 public class EmailService {
+
+    private JavaMailSender javaMailSender;
+
     @JmsListener(destination = "EMAIL_RECIPE_SENDER")
     public void receive(final ObjectMessage message) throws JMSException {
         EmailWithRecipeDto deserialized = (EmailWithRecipeDto) SerializationUtils.deserialize((byte[]) message.getObject());
@@ -18,7 +23,17 @@ public class EmailService {
 
 
 
+    }
 
+    public void send(String emailTo, String subject, String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setFrom(emailConfig.getUsername());
+        mailMessage.setTo(emailTo);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+
+        mailSender.send(mailMessage);
     }
 
 }
