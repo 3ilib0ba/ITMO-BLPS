4d122292f95dd83a1f05d745bb79cc5077cca222
Daniil Fridkin <daniilfridkin@gmail.com>
2023-08-13 20:38:26 +0300

    Trying to add posts and getting all comments

--- Lab-1/src/main/java/evgesha/blps/lab1/camunda/GetAllCommentsDelegate.java
+++ Lab-1/src/main/java/evgesha/blps/lab1/camunda/GetAllCommentsDelegate.java
@@ -1,6 +1,7 @@
 package evgesha.blps.lab1.camunda;
 
 import evgesha.blps.lab1.entity.Comment;
+import evgesha.blps.lab1.dto.CommentUserDto;
 import evgesha.blps.lab1.service.CommentService;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
@@ -17,27 +18,11 @@ import java.util.Map;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class CheckCommentsCronDelegate {
+public class GetAllCommetnsDelegate {
     @Autowired
     private CommentService commentService;
 
-    @JobWorker(type = "cron_check_comments", autoComplete = true)
-    public Map<String, Object> checkAllCommentsForBadWords(final JobClient client, final ActivatedJob job) {
-        log.info("JOB: check comments for bad words");
-
-        Map<String, Object> results = new HashMap<>();
-
-        List<Comment> deletedComments  = commentService.deleteAllCommentsWithBadWords();
-        StringBuilder commentsToString = new StringBuilder("");
-        for (Comment comment : deletedComments) {
-            commentsToString.append(comment.toString()).append(" ");
-        }
-        results.put("deleted_comments", commentsToString.toString());
-
-        return results;
-    }
-
-    @JobWorker(type = "get_all_comments", autoComplete = true)
+    @JobWorker(type = "get_all_comments", autoComplete = false)
     public Map<String, Object> getAllComments(final JobClient client, final ActivatedJob job) {
         log.info("JOB: get all comments");
 
@@ -51,17 +36,6 @@ public class CheckCommentsCronDelegate {
         results.put("all_commetns", commentsToString.toString());
 
         return results;
-    }
-
-    @JobWorker(type = "post_comment", autoComplete = true)
-    public void postComment(final JobClient client, final ActivatedJob job,
-                            @Variable String text,
-                            @Variable Long recipe_id
-    ) {
-        log.info("JOB: post new comment");
-
-        Map<String, Object> results = new HashMap<>();
-        commentService.postComment(new CommentDTO(text, recipe_id))
     }
 
 }
