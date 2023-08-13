fa349b46904dbad057b7a2a758ee6b8e15dc4980
3ilib0ba <evonniy@mail.ru>
2023-08-13 13:47:06 +0300

    LAB-4 hello world with camunda BP(zebee configs in app.props)

---
+++ Lab-1/src/main/java/evgesha/blps/lab1/camunda/HelloDelegate.java
@@ -0,0 +1,28 @@
+package evgesha.blps.lab1.camunda;
+
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class HelloDelegate {
+    @JobWorker(type = "hello_world", autoComplete = true)
+    public Map<String, Object> getArticleIdsToReview(final JobClient client, final ActivatedJob job) {
+        log.info("JOB: hello world!");
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("answer_from_hello_world", "ANSWER FROM SERVER");
+
+        //client.newCompleteCommand(job.getKey()).variables(map);
+        return map;
+    }
+
+}
